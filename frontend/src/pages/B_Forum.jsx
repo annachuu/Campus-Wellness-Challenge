ddcfcafb585ee162ddcf5374837fc5f26cd31f9a
annachuu <annachu211@gmail.com>
2025-06-17 15:49:20 -0600

    Function for user to post

--- frontend/src/pages/B_Forum.jsx
+++ frontend/src/pages/B_Forum.jsx
@@ -9,7 +9,8 @@
 
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useSelector}from 'react-redux'
+import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
 import { 
   Container, 
   Typography, 
@@ -17,27 +18,63 @@ import {
   Box,
   Paper, 
   Avatar,
+  TextField,
   Divider,
   IconButton,
 } from '@mui/material';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
 import FavoriteIcon from '@mui/icons-material/Favorite'
 import '../styles/pages.css'
+import axios from 'axios'
 
 function Forum() {
+  const {user} = useSelector((state) => state.auth)
   const navigate = useNavigate()
+  const [text, setText] = useState('')
   
   return (
     <Container component="main" maxWidth="md" className="page-container" sx={{pt: 10}}>
-       {/* Back Arrow */}
-        <IconButton onClick={() => navigate('/view-challenge')} sx={{position: 'absolute', left: 450}}>
-            <ArrowBackIosNewIcon />
-        </IconButton>
+      {/* Back Arrow */}
+      <IconButton onClick={() => navigate('/view-challenge')} sx={{position: 'absolute', left: 450}}>
+          <ArrowBackIosNewIcon />
+      </IconButton>
 
-        {/* Page Title */}
-        <Typography component="h1" variant="h4" className="page-title" sx={{ mb: 4, color: '#283d3b', mt: -1 }}>
-            Forum Posts
-        </Typography>
+      {/* Page Title */}
+      <Typography component="h1" variant="h4" className="page-title" sx={{ mb: 4, color: '#283d3b', mt: -1 }}>
+          Forum Posts
+      </Typography>
+
+      {/* Write a Post */}
+      <Paper sx={{ p: 5, mb: 5, display: 'flex', gap: 3, alignItems: 'flex-start', width: 700}} >
+        <Box textAlign="center">
+          <Avatar sx={{backgroundColor: '#795663', mx: 'auto'}}>
+            {user.name?.[0] || '?'}
+          </Avatar>
+          <Typography variant="caption">{user.name}</Typography>
+        </Box>
+
+        <TextField
+          multiline fullWidth minRows={3}
+          placeholder="Share your thoughts and experience !"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+
+        <Button
+          variant="contained"
+          sx={{ mt: 1, backgroundColor: '#283D3B'}}
+        >
+          Post
+        </Button>
+      </Paper>
+
+      <Divider sx={{ mb: 3}} />
+
+      {/* List of Posts */}
+      
+
+      {/* Likes */}
+
 
     </Container>
   )
